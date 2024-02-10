cc350cf7c0065e14b6615c7b03317ece1b440710
Eduardo Martinez <eduardo_martinez87@hotmail.com>
2024-02-10 15:12:08 -0600

    feat: Confirmación de Correo

--- src/pages/ConfirmarCuenta.jsx
+++ src/pages/ConfirmarCuenta.jsx
@@ -1,7 +1,60 @@
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import Alerta from "../components/Alerta";
 
 const ConfirmarCuenta = () => {
+
+  const [ cuentaConfirmada, setCuentaConfirmada ] = useState(false);
+  const [ cargando, setCargando ] = useState(true);
+  const [ alerta, setAlerta ] = useState({});
+  
+  const params = useParams();
+  const { id } = params;
+
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      try {
+        const url = `http://localhost:4000/api/usuarios/confirmar/${id}`;
+        const resultado = await axios.get(url);
+
+        setCuentaConfirmada(true);
+
+        setAlerta({
+          msg: resultado.data.msg
+        });
+      } catch (error) {
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true
+        });
+      }
+
+      setCargando(false);
+    }
+
+    confirmarCuenta();
+  }, []);
+
+  const { msg } = alerta;
+  
   return (
-    <div>ConfirmarCuenta</div>
+    <>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Confirma tu Cuenta y Accede {''}<span className="text-black">al Sistema de Administración</span>
+        </h1>
+      </div>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        { !cargando && <Alerta alerta={alerta} /> }
+        { cuentaConfirmada && 
+          <Link 
+            to="/" 
+            className="block text-center my-5 text-gray-500"
+          >Iniciar Sesión</Link>
+        }
+      </div>
+    </>
   )
 }
 
