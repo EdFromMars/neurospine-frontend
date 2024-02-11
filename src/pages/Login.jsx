0125453ece5c4ac808b5f8b07d8dbc376fed4969
Eduardo Martinez <eduardo_martinez87@hotmail.com>
2024-02-10 21:18:51 -0600

    feat: Autenticación de Usuarios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,48 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/clienteAxios";
+import useAuth from "../hooks/useAuth"
 
 const Login = () => {
+  const { setAuth } = useAuth();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alerta, setAlerta] = useState({});
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if([ email, password ].includes('')){
+      return setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      });
+    }
+
+    try {
+      const { data } = await clienteAxios.post('/usuarios/', { email, password });
+
+      localStorage.setItem('neurospinetoken', data.token);
+
+      setAuth( data );
+      
+      navigate('/dashboard');
+
+    } catch (error) {
+      console.log(error.response);
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      });
+    }
+  }
+  
+  const { msg } = alerta;
+  
   return (
     <>
       <div>
@@ -9,12 +51,17 @@ const Login = () => {
         </h1>
       </div>
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form>
+        {msg && <Alerta
+          alerta = { alerta }
+        ></Alerta>}
+        <form onSubmit={ handleSubmit }>
           <div className="my-5">
             <input 
               type="email" 
               placeholder="Email"
               className="border w-full p-3 mt-3"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
             />
           </div>
           <div className="my-5">
@@ -22,6 +69,8 @@ const Login = () => {
               type="password" 
               placeholder="Password"
               className="border w-full p-3 mt-3"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
             />
           </div>
           <input 
