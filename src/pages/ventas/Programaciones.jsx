38abc69f42306f9993e24199d8151ecf43acf372
Eduardo Martinez <eduardo_martinez87@hotmail.com>
2024-10-07 14:48:11 -0600

    fix: Programaciones pasadas mostrándose

--- src/pages/ventas/Programaciones.jsx
+++ src/pages/ventas/Programaciones.jsx
@@ -1,51 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { PlusCircleIcon  } from "@heroicons/react/20/solid";
+import { PlusCircleIcon } from "@heroicons/react/20/solid";
 
 import useProgramacion from "../../hooks/useProgramacion";
 import ProgramacionCard from "../../components/ventas/ProgramacionCard";
-import { useEffect } from "react";
-import { isPast, isToday, isFuture, format } from "date-fns";
+import { isPast, isToday, isFuture, parseISO } from "date-fns";
 
 const Programaciones = () => {
-  const { obtenerProgramaciones, programaciones, setProgramaciones } = useProgramacion();
+  const { obtenerProgramaciones, programaciones } = useProgramacion();
+  const [programacionesFuturas, setProgramacionesFuturas] = useState([]);
+  const [programacionesActivas, setProgramacionesActivas] = useState([]);
+  const [programacionesPasadas, setProgramacionesPasadas] = useState([]);
 
   useEffect(() => {
     obtenerProgramaciones();
   }, []);
-  
-  const programacionesFecha = programaciones.sort((a, b) => {
-    if (a.fechaCirugia < b.fechaCirugia) {
-      return -1;
-    }
-    if (a.fechaCirugia > b.fechaCirugia) {
-      return 1;
-    }
-    return 0;
-  });
 
-  const programacionesFuturas = programaciones.map((programacion) => {
-    const date = new Date(programacion.fechaCirugia.replace(/-/g, '/'))
-    if(isToday(date) || isFuture(date)){
-      return programacion;
-    }
-  });
+  useEffect(() => {
+    if (programaciones.length > 0) {
+      const futuras = [];
+      const activas = [];
+      const pasadas = [];
 
-  const programacionesActivas = programaciones.map((programacion) => {
-    if(programacion.activa === true){
-      return programacion;
-    }
-  });
+      programaciones.forEach((programacion) => {
+        const date = parseISO(programacion.fechaCirugia);
+        if (isToday(date) || isFuture(date)) {
+          futuras.push(programacion);
+        }
+        if (programacion.activa) {
+          activas.push(programacion);
+        }
+        if (isPast(date) && !isToday(date)) {
+          pasadas.push(programacion);
+        }
+      });
 
-  const arrayFilter = array => {
-    return array.filter((item) => {
-      return item !== undefined;
-    });
-  }
-  
-  programaciones.map((programacion) => {
-    const date = new Date(programacion.fechaCirugia.replace(/-/g, '/'))
-  });
+      setProgramacionesFuturas(futuras);
+      setProgramacionesActivas(activas);
+      setProgramacionesPasadas(pasadas);
+    }
+  }, [programaciones]);
 
   return (
     <>
@@ -56,39 +50,51 @@ const Programaciones = () => {
           </h2>
         </div>
         <div className="mt-4 flex md:ml-4 md:mt-0">
-        <Link
-          to="agregar-programacion"
-          className="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Agregar Programación
-          <PlusCircleIcon  className="-mr-0.5 h-5 w-5" aria-hidden="true" />
-        </Link>
+          <Link
+            to="agregar-programacion"
+            className="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Agregar Programación
+            <PlusCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
+          </Link>
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row gap-12">
-        <div role="list" className="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl w-1/2">
+      <div className="flex flex-col md:flex-row gap-12 mb-12">
+        <div role="list" className="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl w-full md:w-1/2">
           <h2 className="text-xl font-semibold leading-7 text-gray-900 p-6">Próximas</h2>
           <ul>
-            {arrayFilter(programacionesFuturas).map((programacion) => (
+            {programacionesFuturas.map((programacion) => (
               <ProgramacionCard 
-              key={programacion._id}
-              programacion={programacion}
+                key={programacion._id}
+                programacion={programacion}
               />
             ))}
           </ul>
         </div>
-        <div role="list" className="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl w-1/2">
+        <div role="list" className="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl w-full md:w-1/2">
           <h2 className="text-xl font-semibold leading-7 text-gray-900 p-6">Activas</h2>
           <ul>
-            {arrayFilter(programacionesActivas).map((programacion) => (
+            {programacionesActivas.map((programacion) => (
               <ProgramacionCard 
-              key={programacion._id}
-              programacion={programacion}
+                key={programacion._id}
+                programacion={programacion}
               />
             ))}
           </ul>
         </div>
+      </div>
+
+      <div role="list" className="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl w-full">
+        <h2 className="text-xl font-semibold leading-7 text-gray-900 p-6">Programaciones Pasadas</h2>
+        <ul>
+          {programacionesPasadas.map((programacion) => (
+            <ProgramacionCard 
+              key={programacion._id}
+              programacion={programacion}
+            />
+          ))}
+        </ul>
       </div>
     </>
   )
